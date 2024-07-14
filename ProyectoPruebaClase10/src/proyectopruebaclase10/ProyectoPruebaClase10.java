d52859e0668dd77e1b76a5b67ffdfcee468f22de
Katherine Gomez Alderete <katherinealdereteg@gmail.com>
2024-07-13 23:45:26 -0400

    primer commit

---
+++ ProyectoPruebaClase10/src/proyectopruebaclase10/ProyectoPruebaClase10.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectopruebaclase10;
+
+
+public class ProyectoPruebaClase10 {
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
