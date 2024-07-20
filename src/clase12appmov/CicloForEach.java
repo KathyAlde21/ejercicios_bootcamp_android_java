4388ed3748624bf9b67047120ddaccf43a2286c5
Katherine Alderete Gomez <katherinealdereteg@gmail.com>
2024-07-20 18:45:38 -0400

    clase 12

--- src/clase12appmov/CicloForEach.java
+++ src/clase12appmov/CicloForEach.java
@@ -4,8 +4,11 @@ public class CicloForEach {
     public static void main(String[] args) {
         
        //se especializa en recorrer datos
+       String[] ciudades = {"Santiago", "Concepción", "Antofagasta", "Talca", "Punta Arenas"};
        
-       
+       for (String e : ciudades){ //e por element, puedo poner la letra o frase que quiera
+           System.out.println(e);
+       }
         
     }
 }
