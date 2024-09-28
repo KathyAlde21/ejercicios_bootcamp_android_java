7f9aa0cf7b593751b90986e5e0dd2dbc49f3d098
Katherine Alderete Gomez <katherinealdereteg@gmail.com>
2024-09-28 20:04:03 -0300

    actualizando

---
+++ src/clase17appmov/POO.java
@@ -0,0 +1,15 @@
+
+package clase17appmov;
+
+import java.util.Scanner;
+
+//ProgramacionOrientadaAObjetos
+public class POO {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        
+        
+        
+        
+    }
+}
