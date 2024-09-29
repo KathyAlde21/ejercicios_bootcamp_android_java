998f7aebb672e6ef3fdef4afb965c7d1d7006abe
Katherine Alderete Gomez <katherinealdereteg@gmail.com>
2024-09-29 20:39:13 -0300

    clase 17

--- src/clase17appmov/POO.java
+++ src/clase17appmov/POO.java
@@ -8,8 +8,51 @@ public class POO {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         
+     /* CLASES */
+     //los atributos que recibe pueden ser ingresados o forzados preescritos
+     //ej:
+    /*    class Auto { //clase
+         
+            String marca;
+            String color;
+            double velocidad;
+        }
+     */
+    
+    // -----------------------------------------------------------------------
+     
+     /* METODO */
+     //puede pertenecer a una clase u objeto y en general cambian uno de los 
+     //atributos 
+     //ej:
+     /*
+        class Auto { //clase
+         
+            String marca;
+            String color;
+            double velocidad;
+            
+            void acelerar(){ //metodo
+                velocidad++;
+            }
+            
+            void frenar(){ //metodo
+                velocidad = 0;
+            }         
+        }
+     */
         
-        
-        
+    // -----------------------------------------------------------------------
+    
+    /* MODIFICADORES DE ACCESO */
+    //determina desde que clase se puede acceder a un elemento
+    //sino se determina queda por defecto, solo se puede acceder desde su mismo paquete
+    
+    //PUBLIC => puedo acceder desde cualquier clase
+    
+    
+    //PRIVATE => indica que elementos pueden ser utilizados dentro de la clase
+    
+    
     }
 }
